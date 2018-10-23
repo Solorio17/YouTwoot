41634db9268ca9d62e814d071419c44d511a2985
Jason Solorio <jasonsoloriom@gmail.com>
2018-10-23 16:56:15 -0600

    Attempt at a custom / bootstrap form validation.

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 
+
 class SignUp extends Component{
   constructor(props){
     super(props);
@@ -7,18 +8,23 @@ class SignUp extends Component{
     this.state = {
       username: '',
       email: '',
-      password: ''
+      password: '',
+      emailValidation: true
     }
   }
+
+
 handleUsernameChange = (e) =>{
   this.setState({username: e.target.value});
 };
 handleEmailChange = (e) =>{
   this.setState({email: e.target.value});
+
 };
 handlePasswordChange = (e) =>{
   this.setState({password: e.target.value})
 };
+
 handleSignUp = () =>{
   fetch('http://localhost:3000/auth/register', {
     method: 'POST',
@@ -37,6 +43,32 @@ handleSignUp = () =>{
   });
 };
 
+validateForm = () =>{
+
+  const validateUsername = () =>{
+    if(this.state.username == ''){
+    }
+  }
+  validateUsername()
+
+  const validateEmail = () =>{
+    if(this.state.email == ''){
+      this.setState({emailValidation: false})
+    }
+    
+  }
+  validateEmail()
+
+  const validatePassword = () =>{
+    if(this.state.password == ''){
+    }
+  }
+  validatePassword()
+
+  if(this.state.emailValidation)
+    this.handleSignUp()
+}
+
   render(){
     return(
       <div className="container">
@@ -45,36 +77,48 @@ handleSignUp = () =>{
             <hr/>
 
             <div className="form-group">
-              <label className="control-label">Username</label>
-                <input type="text"
-                       name="username"
-                       className="form-control"
-                       placeholder="Username"
-                       value={this.state.username}
-                       onChange={this.handleUsernameChange}></input>
-            </div>
+              <div>
+                <label>Username</label>
+                <input
+                  type="text"
+                  id="username"
+                  placeholder="Username"
+                  name="username"
+                  className="form-control"
+                  value={this.state.username}
+                  onChange={this.handleUsernameChange}
+                  ></input>
+              </div>
+              <hr></hr>
+              <div>
+                <label >Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Email"
+                  name="email"
+                  className="form-control"
+                  value={this.state.email}
+                  onChange={this.handleEmailChange}
+                  ></input>
+                {this.state.emailValidation == false ? (<p>Please enter a valid email address</p>) : ''}
+              </div>
+              <hr></hr>
+              <div>
+                <label >Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="Password"
+                  name="password"
+                  className="form-control"
+                  value={this.state.password}
+                  onChange={this.handlePasswordChange}
+                  ></input>
+              </div>
 
-            <div className="form-group">
-              <label className="control-label">Email</label>
-                <input type="text"
-                       name="email"
-                       className="form-control"
-                       placeholder="Email"
-                       value={this.state.email}
-                       onChange={this.handleEmailChange}></input>
             </div>
-
-            <div className="form-group">
-              <label className="control-label">Password</label>
-                <input type="password"
-                       name="password"
-                       className="form-control"
-                       placeholder="Password"
-                       value={this.state.password}
-                       onChange={this.handlePasswordChange}></input>
-            </div>
-
-            <button className="btn btn-info" type="button" onClick={this.handleSignUp}>Join the fun!</button>
+            <button className="btn btn-info" type="button" onClick={this.validateForm}>Join the fun!</button>
           </form>
       </div>
     )
